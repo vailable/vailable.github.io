e5ec16527ecc7f2ceef38168fe06ba3e27b00345
John Moffitt <39016062+whatsyourgithub@users.noreply.github.com>
2020-08-29 21:49:14 -0700

    init

---
+++ precache-manifest.b416fd751eb154aa73adede6d657efa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4407dc21498a0b1ad55d700c796651fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f21fff2c5e59af0aa36e",
+    "url": "/static/js/2.02a10c62.chunk.js"
+  },
+  {
+    "revision": "26bfc857ea6489081ae6b3300fafb157",
+    "url": "/static/js/2.02a10c62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bcf45773b12d5d817b4",
+    "url": "/static/js/main.4339f05f.chunk.js"
+  },
+  {
+    "revision": "b214a7f1ea897e57a676",
+    "url": "/static/js/runtime-main.ca69096e.js"
+  }
+]);
